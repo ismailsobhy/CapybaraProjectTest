cd1aad8f6efe09dc92b69d60c5c2f44549a50215
Jacopo <beschi.jacopo@gmail.com>
2018-12-13 16:51:20 +0100

    Base setup of testing app

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+## Setup
+
+1. App1
+* bin/setup
+* Run tests: bundle exec rspec
+
+2. App2
+* bundle install
+* rails s -p 3001
---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'User signs in' do
+  scenario 'with valid credentials' do
+  end
+
+  scenario 'with invalid credentials' do
+  end
+end
+
+feature 'User signs out' do
+  scenario 'redirects the user to the signin page' do
+  end
+end
+
