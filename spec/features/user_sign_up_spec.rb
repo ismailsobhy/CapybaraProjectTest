2cd82b9ae08cd9a83e79e4b55af0555a787efd02
ismail <ismailsobhy@gmail.com>
2021-03-23 14:15:02 +0200

    sign up

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature 'User signs up' do
-  scenario 'with valid data' do
+  let(:user) { create(:user) }
+  scenario 'with valid data non existing user' do
     visit(new_user_session_path)
     click_on('Registration')
 
@@ -12,5 +13,54 @@ feature 'User signs up' do
     click_on('Sign up')
     
     expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+  scenario 'with valid data (existing user)' do
+
+    visit(new_user_session_path)
+    click_on('Registration')
+
+    fill_in('user_email', with: user.email)
+    fill_in('user_password',with: user.password)
+    fill_in('user_password_confirmation',with: user.password)
+    
+    click_on('Sign up')
+    
+    expect(page).to have_content('Email has already been taken')
+  end
+  scenario 'with non matching passwords' do
+
+    visit(new_user_session_path)
+    click_on('Registration')
+
+    fill_in('user_email', with: user.email)
+    fill_in('user_password',with: user.password)
+    fill_in('user_password_confirmation',with: user.password+"123")
+    
+    click_on('Sign up')
+    
+    expect(page).to have_content('Password confirmation doesn\'t match Password')
+  end
+  scenario 'with non matching passwords' do
+
+    visit(new_user_session_path)
+    click_on('Registration')
+ 
+    click_on('Sign up')
+    
+    expect(page).to have_content('Password can\'t be blank')
+    expect(page).to have_content('Email can\'t be blank')
+  end
+  scenario 'with non shor password' do
+
+    visit(new_user_session_path)
+    click_on('Registration')
+
+    fill_in('user_email', with: user.email)
+    fill_in('user_password',with: "123")
+    fill_in('user_password_confirmation',with: "123")
+    
+    click_on('Sign up')
+    
+    expect(page).to have_content('Password is too short (minimum is 8 characters)')
   end
 end
