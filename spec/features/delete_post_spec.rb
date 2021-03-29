908db99927d8ecb7b668240792cc5a7ef839ee4f
ismail <ismailsobhy@gmail.com>
2021-03-29 13:15:51 +0200

    edits to create comment

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -4,7 +4,7 @@ feature 'User deletes post' do
   let!(:post) { create(:post, user: user) }
     scenario 'using search and click on button' do
       visit(new_user_session_path)
-      login(post.user);
+      login(user);
       title=post.title
       fill_in('search_query', with: post.description)
    
